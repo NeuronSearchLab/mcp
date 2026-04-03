d45efc445fdbfef6496d8981eedc75aae666521a
jaredpestugia <jeddpestugia@live.com>
2026-04-03 10:06:53 +0100

    Add .gitignore, remove node_modules and dist from tracking

--- dist/server.d.ts
+++
@@ -1,4 +0,0 @@
-import { Server } from '@modelcontextprotocol/sdk/server/index.js';
-import { NeuronClient } from './client.js';
-export declare function createServer(client: NeuronClient): Server;
-//# sourceMappingURL=server.d.ts.map
